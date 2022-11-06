b3c837eaac037a093d7b32b09b46abbe1131cd90
Minh Duy <minhduy140799@gmail.com>
2022-11-06 19:53:13 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import 'react-app-polyfill/stable'
+import { render } from 'react-dom'
 import 'core-js'
 import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './store/index'
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+// )
+
+const root = document.getElementById('root')
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+render(
   <Provider store={store}>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>,
+  root,
 )
 
 // If you want to start measuring performance in your app, pass a function
